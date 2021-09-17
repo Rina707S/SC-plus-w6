8021362e8055c31309faae489453b0dc576522ff
Rina707S <88797166+Rina707S@users.noreply.github.com>
2021-09-17 23:24:04 +0900

    Removed console.log

--- src/script.js
+++ src/script.js
@@ -67,7 +67,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "c9aaec0a4189697bb2f4013911b89486";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
