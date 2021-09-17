cdf040e3212c07fe4fce8784ba06891d104f226f
Rina707S <88797166+Rina707S@users.noreply.github.com>
2021-09-17 18:57:44 +0900

    Added api data for forecast

--- src/script.js
+++ src/script.js
@@ -11,7 +11,7 @@ function formatDate(timestamp) {
   let days = [
     "Sunday",
     "Monday",
-    "Tyuesday",
+    "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
@@ -21,29 +21,45 @@ function formatDate(timestamp) {
   return `${day} ${hour}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed", "Thu"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `  
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `  
       <div class="col-2">
-        <div class="forecast-date">${day}</div>
+        <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="https://ssl.gstatic.com/onebox/weather/48/cloudy.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           with="36"
         />
         <div class="forecast-temperature">
-          <span class="forecast-max"> 22°C</span>
-          <span class="forecast-min"> 18°C</span>
+          <span class="forecast-max"> ${Math.round(
+            forecastDay.temp.max
+          )}°C</span>
+          <span class="forecast-min"> ${Math.round(
+            forecastDay.temp.min
+          )}°C</span>
         </div>
           </div>
 `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
